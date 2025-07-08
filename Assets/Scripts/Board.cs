04c2978cf566823825cf413b2dac6f35f431ae58
RoyBolter <ro465439@ucf.edu>
2025-07-08 17:35:35 -0400

    Update

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,17 +6,24 @@ public class Board : MonoBehaviour {
 
     [SerializeField]
     public Tile unknown, empty, mine, mineExploded, flag, one, two, three, four, five, six, seven, eight;
-    
-    private Cell[,] state;
-    public Board board;
-    
-    public int width;
-    public int height;
-    public int xPos;
-    public int yPos;
-    public int mines;
-    
-    private const int DEBUG = 0;
+
+    private int xPos, yPos;
+
+    public int getX() {
+        return xPos;
+    }
+
+    public int getY() {
+        return yPos;
+    }
+
+    public void setX(int x) {
+        xPos = x;
+    }
+
+    public void setY(int y) {
+        yPos = y;
+    }
 
     private void Awake() {
         tilemap = GetComponent<Tilemap>();
@@ -59,69 +66,6 @@ public class Board : MonoBehaviour {
             case 7: return seven;
             case 8: return eight;
             default: return null;
-        }
-    }
-    
-    public void CreateBoard(int xPos, int yPos, int width, int height) {
-        state = new Cell[width, height];
-
-        // Initialize Board
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                Cell cell = new Cell();
-                cell.position = new Vector3Int(x + xPos, y + yPos, 0);
-                cell.type = Cell.Type.Empty;
-                state[x, y] = cell;
-
-                if (DEBUG == 1) state[x, y].isClicked = true;
-            }
-        }
-
-        board.SetTile(state);
-    }
-
-    public void GenerateMines(int safeX, int safeY, int xPos, int yPos) {
-        for (int i = 0; i < mines; i++) {
-            int x = UnityEngine.Random.Range(0, width) + xPos;
-            int y = UnityEngine.Random.Range(0, height) + yPos;
-
-            if (x >= safeX - 1 && x <= safeX + 1 && y >= safeY - 1 && y <= safeY + 1) {
-                i--;
-                continue;
-            }
-
-            if (state[x, y].type != Cell.Type.Mine)
-                state[x, y].type = Cell.Type.Mine;
-            else i--;
-
-            if (DEBUG == 1) state[x, y].isClicked = true;
-        }
-    }
-
-    public void GenerateNumbers() {
-        // For each cell
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                // Skip the cell if it is a mine
-                if (state[x, y].type == Cell.Type.Mine) continue;
-                // Count the number of mines around the cell
-                int count = 0;
-                for (int i = -1; i < 2; i++) {
-                    for (int j = -1; j < 2; j++) {
-                        if (x + i < 0 || x + i >= width || y + j < 0 || y + j >= height) continue;
-                        if (state[x + i, y + j].type == Cell.Type.Mine) count++;
-                    }
-                }
-
-                // Set the number of mines around the cell
-                state[x, y].type = Cell.Type.Number;
-                state[x, y].number = count;
-
-                // Make empty tiles empty
-                if (count == 0) state[x, y].type = Cell.Type.Empty;
-
-                if (DEBUG == 1) state[x, y].isClicked = true;
-            }
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour {
     private Board board;
     private Cell[,] state;
 
-    private const int DEBUG = 1;
+    private const int DEBUG = 0;
 
     private void Awake() {
         board = GetComponentInChildren<Board>();
@@ -17,13 +17,85 @@ public class GameManager : MonoBehaviour {
 
     private void Start() {
         //Set up Board
-        board.CreateBoard(3,3,width, height);
+        CreateBoard(0,0,width, height);
+        CreateBoard(16, 0, width, height);
 
         //Set up Camera
         Camera.main.transform.position = new Vector3(width / 2f, height / 2f, -10f);
     }
 
-    
+    private void CreateBoard(int xPos, int yPos, int width, int height) {
+        Cell[,] state;
+        state = new Cell[width, height];
+
+        // Initialize Board
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Cell cell = new Cell();
+                cell.position = new Vector3Int(x + xPos, y + yPos, 0);
+                cell.type = Cell.Type.Empty;
+                state[x, y] = cell;
+
+                if (DEBUG == 1) state[x, y].isClicked = true;
+            }
+        }
+
+        // Generate Mines
+        GenerateMines(xPos, yPos, width, height, state);
+        GenerateNumbers(state);
+        board.SetTile(state);
+    }
+
+    private void GenerateMines(int safeX, int safeY, int xPos, int yPos, Cell[,] state) {
+        // For each mine
+        for (int i = 0; i < mines; i++) {
+            // Generate a random position
+            int x = UnityEngine.Random.Range(0, width);
+            int y = UnityEngine.Random.Range(0, height);
+
+            // Skip the cell if it is in the safe zone
+            if (x >= safeX - 1 && x <= safeX + 1 && y >= safeY - 1 && y <= safeY + 1) {
+                i--;
+                continue;
+            }
+
+            // Set the cell to be a mine
+            if (state[x, y].type != Cell.Type.Mine)
+                state[x, y].type = Cell.Type.Mine;
+            else i--;
+
+            // Set the cell to be clicked if in debug mode
+            if (DEBUG == 1) state[x, y].isClicked = true;
+        }
+    }
+
+    private void GenerateNumbers(Cell[,] state) {
+        // For each cell
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                // Skip the cell if it is a mine
+                if (state[x, y].type == Cell.Type.Mine) continue;
+                // Count the number of mines around the cell
+                int count = 0;
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (x + i < 0 || x + i >= width || y + j < 0 || y + j >= height) continue;
+                        if (state[x + i, y + j].type == Cell.Type.Mine) count++;
+                    }
+                }
+
+                // Set the number of mines around the cell
+                state[x, y].type = Cell.Type.Number;
+                state[x, y].number = count;
+
+                // Make empty tiles empty
+                if (count == 0) state[x, y].type = Cell.Type.Empty;
+
+                if (DEBUG == 1) state[x, y].isClicked = true;
+            }
+        }
+    }
+
     private void Update() {
         if (Input.GetMouseButtonDown(1)) {
             Flag();
@@ -33,21 +105,19 @@ public class GameManager : MonoBehaviour {
             if (!clickedOnce) {
                 Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Vector3Int cellPos = board.tilemap.WorldToCell(mousePos);
-                board.GenerateMines(cellPos.x, cellPos.y, 0,0);
-                board.GenerateNumbers();
+                //GenerateMines(cellPos.x, cellPos.y, 0,0);
+                //GenerateNumbers();
                 clickedOnce = true;
             }
 
             Click();
         }
 
-        
         if (Input.GetKeyDown("r")) {
             clickedOnce = false;
-            board.CreateBoard(0,0,width, height);
+            CreateBoard(0,0,width, height);
             return;
         }
-        
     }
 
     private void Flag() {
